882fd34ab59290bb113ecaf48b30c5ece7751b39
Khallil <khallil.doudou@epitech.eu>
2018-02-16 20:10:08 +0800

    read seq_example

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 
 import tensorflow as tf
 
-filenames = ["/home/doudou/Documents/Listen-Project/audioset_v1_embeddings/0.tfrecord"]
+# On lit le fichier et on le transforme en objet Example
+reader = tf.python_io.tf_record_iterator('/home/doudou/Documents/Listen-Project/tfrecords/_0.tfrecord')
+my_readed_examples = [tf.train.SequenceExample().FromString(example_str)
+        for example_str in reader]
 
-def _parse_function(example_proto):
-    features = {"video_id":tf.FixedLenFeature((), tf.bytes_list: )}
-
-    }
-    parsed_features = tf.parse_single_sequence_example(example_proto,)
-    return parsed_features
-
-dataset = tf.data.TFRecordDataset(filenames)
-print "retour de TFRecordDataset : "
-print dataset
-
-dataset = dataset.map(_parse_function)
-print dataset
+#my_n_example=my_readed_examples[0]
+for seq_example in my_readed_examples:
+    print seq_example
